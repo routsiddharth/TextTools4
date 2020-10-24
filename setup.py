7bc74e56882dbe65df4a68f7bffd31d9b98d3760
routsiddharth <72611815+routsiddharth@users.noreply.github.com>
2020-10-24 15:50:55 +0300

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'TextTools4',
   packages = ['TextTools4'],
-  version = '0.1',
+  version = '0.1.2',
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package to provide data and use tools on a text.',   # Give a short description about your library
   author = 'Siddharth Rout',                   # Type in your name
   author_email = 'routsiddharth2911@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/routsiddharth/TextTools4',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/routsiddharth/TextTools4/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/routsiddharth/TextTools4/archive/v0.1.2.tar.gz', # I explain this later on
+  long_description = long_description,
+  long_description_content_type="text/markdown",
   keywords = ['DATA', 'ANALYSIS', 'TEXT', 'WORDS', 'CHART', 'GRAPH', 'TOOLS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'docx',
